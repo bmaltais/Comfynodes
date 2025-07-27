79ebff763036f0de76c50382932c41a245ac71d2
bmaltais <bernard@ducourier.com>
2025-07-27 10:59:21 -0400

    Update title

--- UpscaleByModelToTotalPixels/__init__.py
+++ UpscaleByModelToTotalPixels/__init__.py
@@ -1,8 +1,8 @@
 """
 @author: ominiman
-@title: Upscale Image To Total Pixels
-@nickname: Upscale Image To Total Pixels
-@description: This custom node allow upscaling an image to a desired total pixel count.
+@title: Upscale Image To Total Pixels (using Model)
+@nickname: Upscale Image To Total Pixels (using Model)
+@description: This custom node allow upscaling an image to a desired total pixel count using a specified model. If the input image is larger than the target, it will be downscaled to fit the total pixel count without getting upscaled by the model first.
 """
 
 from .nodes import UpscaleImageToTotalPixels
@@ -12,5 +12,5 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "UpscaleImageToTotalPixels": "Upscale Image To Total Pixels"
+    "UpscaleImageToTotalPixels": "Upscale Image to Total Pixels (using Model)"
 }
